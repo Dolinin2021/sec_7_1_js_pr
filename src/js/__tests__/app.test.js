0971d35fb7b2056b1f526a88805688882a8ada23
Dolinin2021 <programmistdolinin@mail.ru>
2023-02-10 18:52:11 +0600

    modified app.test.js file

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,61 +1,25 @@
 import Validator from '../app';
 
-test('correct input', () => {
+test('correct data entry', () => {
   const check = new Validator();
   const result = check.validateUsername('Ilya-170_Dolinin');
   expect(result).toEqual(true);
 });
 
-test('russian words', () => {
+test.each([{
+  data: [
+    'Илья-170_Долинин',
+    '170-Ilya_Dolinin',
+    'Ilya_Dolinin-170',
+    '_Ilya_Dolinin',
+    'Ilya_Dolinin_',
+    'Ilya_2000_Dolinin',
+    '-Ilya_Dolinin',
+    'Ilya_Dolinin-',
+    'Ilya&Dolinin',
+  ],
+}])('incorrect data entry', ({ data }) => {
   const check = new Validator();
-  const result = check.validateUsername('Илья-170_Долинин');
-  expect(result).toEqual(false);
-});
-
-test('numbers are at the beginning of the word', () => {
-  const check = new Validator();
-  const result = check.validateUsername('170-Ilya_Dolinin');
-  expect(result).toEqual(false);
-});
-
-test('numbers are at the end of the word', () => {
-  const check = new Validator();
-  const result = check.validateUsername('Ilya_Dolinin-170');
-  expect(result).toEqual(false);
-});
-
-test('underscores are at the beginning of a word', () => {
-  const check = new Validator();
-  const result = check.validateUsername('_Ilya_Dolinin');
-  expect(result).toEqual(false);
-});
-
-test('underscores are at the end of a word', () => {
-  const check = new Validator();
-  const result = check.validateUsername('Ilya_Dolinin_');
-  expect(result).toEqual(false);
-});
-
-test('more than four digits in a row', () => {
-  const check = new Validator();
-  const result = check.validateUsername('Ilya_2000_Dolinin');
-  expect(result).toEqual(false);
-});
-
-test('dash is at the beginning of a word', () => {
-  const check = new Validator();
-  const result = check.validateUsername('-Ilya_Dolinin');
-  expect(result).toEqual(false);
-});
-
-test('dash is at the end of a word', () => {
-  const check = new Validator();
-  const result = check.validateUsername('Ilya_Dolinin-');
-  expect(result).toEqual(false);
-});
-
-test('extra characters', () => {
-  const check = new Validator();
-  const result = check.validateUsername('Ilya&Dolinin');
+  const result = check.validateUsername(data);
   expect(result).toEqual(false);
 });
